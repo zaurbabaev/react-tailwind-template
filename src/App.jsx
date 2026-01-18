c4abe2478a94dae8a751eb5c122b62b039e76786
Zaur <zaurik87@gmail.com>
2026-01-18 12:59:22 +0400

    Initial Vite + React + Tailwind 4.1 + Prettier template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 p-4 text-center text-[28px] font-bold text-white">
+      Tailwind 4.1 + React + Prettier Template ✅
+    </div>
+  );
+}
+
+export default App;
